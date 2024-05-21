4edf9a92eafaf9de27f02ee583c43ab23326448b
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-21 11:01:21 -0400

    Create target sum function and add comments as directed

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      } 
+    }
+  } return false
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
+//Quadratic Time: O(n²)
+// or Exponential? 
 
 /* 
   Add your pseudocode here
+  Take array
+  For each number in array 
+  Grab each index
+  For each index get second index +1
+  Add each array[i] + array[i]+1
+  If number is === to target number 
+  return true
+  Else false
 */
 
 /*
   Add written explanation of your solution here
+  The given array needs to have each of its elements added against each other 
+  to determine whether the sum is equal to the target
+  You need to loop through the array to access the index of each element
+  then you'll need to nest a 2nd loop to access access the current index by adding one to its iteration
+  once you have access to the correct indices you can add them together and check for equality to the target
+  if the sum is equal to the target it will return true otherwise it will return false
 */
 
 // You can run `node index.js` to view these console logs
